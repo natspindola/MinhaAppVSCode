2e2a5bad9d4cac8ea8bc2b5aa27c6f6a77898c8c
Perk <natspindola@hotmail.com>
2022-05-18 19:50:31 -0300

    middleware extension

--- MeuMiddleware.cs
+++ MeuMiddleware.cs
@@ -22,3 +22,11 @@ public class MeuMiddleware
     }
 
 }
+
+public static class MeuMiddlewareExtension
+{
+    public static IApplicationBuilder UseMeuMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<MeuMiddleware>();
+    }
+}
